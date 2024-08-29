a8bda1fc2b0d2b55842643074a254a3fed809f38
BrettWilsonBDW <brett.david.wilson.bdw@outlook.com>
2024-08-29 14:28:02 +0200

    fixed bug in sdl3 example main

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,40 @@
 #include <iostream>
 #include "SDL3/SDL.h"
 
-int main()
+int main(int argc, char *argv[])
 {
-	SDL_Window *window;
-	SDL_Renderer *renderer;
-	SDL_Event event;
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    SDL_Event event;
 
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
-	{
-		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s", SDL_GetError());
-		return 3;
-	}
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s", SDL_GetError());
+        return 3;
+    }
+
+    if (!SDL_CreateWindowAndRenderer("Hello SDL", 320, 240, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create window and renderer: %s", SDL_GetError());
+        return 3;
+    }
 
 	const int linked = SDL_GetVersion();
-
 	std::cout << "SDL version: " << SDL_VERSIONNUM_MAJOR(linked) << "." << SDL_VERSIONNUM_MINOR(linked) << "." << SDL_VERSIONNUM_MICRO(linked) << std::endl;
 
-	if (SDL_CreateWindowAndRenderer("Hello SDL", 320, 240, SDL_WINDOW_RESIZABLE, &window, &renderer))
-	{
-		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create window and renderer: %s", SDL_GetError());
-		return 3;
-	}
+    while (1) {
+        SDL_PollEvent(&event);
+        if (event.type == SDL_EVENT_QUIT) {
+            break;
+        }
 
-	while (1)
-	{
-		SDL_PollEvent(&event);
-		if (event.type == SDL_EVENT_QUIT)
-		{
-			break;
-		}
+        SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
 
-		SDL_RenderPresent(renderer);
-		SDL_RenderClear(renderer);
-	}
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
 
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
+    SDL_Quit();
 
-	SDL_Quit();
-
-	return 0;
+    return 0;
 }
