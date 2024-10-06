68164b0fd3bf00dd349d821a15da475c3cb87aae
BrettWilsonBDW <brett.david.wilson.bdw@outlook.com>
2024-10-06 14:04:23 +0200

    Added support for sdl3 emcc

--- tools/build-sdl2-emcc.bat
+++ tools/build-sdl2-emcc.bat
@@ -10,14 +10,14 @@ cd ..\build-em
 
 IF "%1"=="async" (
     echo using asyncify
-    call emcmake cmake .. -DPLATFORM=Web -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXECUTABLE_SUFFIX=".html" -DASYNCIFY=1
+    call emcmake cmake .. -DPLATFORM=Web -DUSE_SDL3=OFF -DUSE_SDL2=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXECUTABLE_SUFFIX=".html" -DASYNCIFY=1
 ) ELSE IF "%1"=="debug" (
-    call emcmake cmake .. -DPLATFORM=Web -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXECUTABLE_SUFFIX=".html"
+    call emcmake cmake .. -DPLATFORM=Web -DUSE_SDL3=OFF -DUSE_SDL2=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXECUTABLE_SUFFIX=".html"
 ) ELSE IF NOT "%1"=="" (
     echo Unknown argument: %1
     exit /b 1
 ) ELSE (
-    call emcmake cmake .. -DPLATFORM=Web -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXECUTABLE_SUFFIX=".html"
+    call emcmake cmake .. -DPLATFORM=Web -DUSE_SDL3=OFF -DUSE_SDL2=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXECUTABLE_SUFFIX=".html"
 )
 
 call emmake make
